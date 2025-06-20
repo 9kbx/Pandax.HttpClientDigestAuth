68df9d701f35495bde509eb7a3e454906da5e4b5
9kbx <550591004@qq.com>
2025-06-20 13:59:42 +0800

    小调整

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -4,8 +4,11 @@ using Pandax.HttpClientDigestAuth;
 var username = "root";
 var password = "root";
 
+Console.Write("type your IP address:");
+var ip = Console.ReadLine()!.Trim();
+
 var client = new HttpClient();
-var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001");
+var request = new HttpRequestMessage(HttpMethod.Get, $"http://{ip}/cgi-bin/get_system_info.cgi");
 var response = await client.SendWithDigestAuthAsync(request, username, password);
 Console.WriteLine(response.StatusCode);
 Console.WriteLine(await response.Content.ReadAsStringAsync());
